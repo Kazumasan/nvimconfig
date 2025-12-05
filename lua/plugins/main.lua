c9b62b4e785b7bfa562d77d64b04bb9e96c2b652
Kazumasan <19864466+Kazumasan@users.noreply.github.com>
2025-12-05 20:32:10 +0100

    new working

--- lua/plugins/main.lua
+++ lua/plugins/main.lua
@@ -4,10 +4,25 @@ return {
     "folke/tokyonight.nvim",
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
+    -- config = function()
       -- load the colorscheme here
-      vim.cmd([[colorscheme tokyonight]])
-    end,
+      -- vim.cmd([[colorscheme tokyonight]])
+    -- end,
+  },
+  { 
+    "EdenEast/nightfox.nvim",
+    config = function()
+      require("nightfox").setup({
+        palettes = {
+          carbonfox = {
+            bg0 = "#222d3d",
+            bg1 = "#0d1117",
+            bg2 = "#222d3d"
+          }
+        }
+      })
+      vim.cmd("colorscheme carbonfox")
+    end
   },
   {
     "ggandor/leap.nvim",
